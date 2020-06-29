3e3f8fd0b77bcefa0adf86e80c344c68ea4f31df
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-29 19:33:24 +0200

    Moved css out of js ;-)

--- edit/js/app.js
+++ edit/js/app.js
@@ -107,55 +107,7 @@ function start() {
 
 function createWidget() {
 
-    document.head.innerHTML += `<style>
-    .ww-widget{background-color:white;position:fixed;bottom:0;right:0;margin:25px;width:350px;min-height:400px;border-radius:6px;overflow:hidden;text-align:center;box-shadow: 0 11px 15px -7px rgba(0, 0, 0, 0.2), 0 24px 38px 3px rgba(0, 0, 0, 0.14), 0 9px 46px 8px rgba(0, 0, 0, 0.12);box-sizing:border-box;}
-    .ww-header{height:160px;background-image:url(edit/img/mountain1.jpg);background-size:cover;}
-    .ww-content{padding: 20px; min-height: 400px;}
-    .ww-footer{padding: 15px;border-top:1px solid #DDD;position: absolute;bottom:0;left:0;width:100%;box-sizing:border-box;}
-    .ww-close{float: right; font-size: 25px; padding:15px; padding-top: 5px;color:white;cursor:pointer;}
-    .ww-widget p{font-family: Helvetica, sans-serif;}
-    .ww-widget h1, .ww-widget h2, .ww-widget h3, .ww-widget h4, .ww-button{font-family: Helvetica, sans-serif; text-transform: uppercase;}
-    .ww-button{display:block;text-align:center;padding:10px 15px;background-color: #20bf6b;border-radius: 4px;color:white;text-decoration: none;}
-    .ww-widget{opacity: 0;animation-name: bounceIn;animation-duration: 250ms;animation-timing-function: linear;animation-fill-mode: forwards;}
-    .closing{animation-name: bounceOut;animation-duration: 250ms;animation-timing-function: linear;animation-fill-mode: forwards;}
-    @keyframes bounceIn{
-      0%{
-        opacity: 0;
-        transform: scale(0.3) translate3d(0,0,0);
-      }
-      100%{
-        opacity: 1;
-        transform: scale(1) translate3d(0,0,0);
-      }
-    }
-    @keyframes bounceOut{
-      0%{
-        opacity: 1;
-        transform: scale(1) translate3d(0,0,0);
-      }
-      20%{
-        transform: scale(1.1) translate3d(0,0,0);
-      }
-      100%{
-        opacity: 0;
-        transform: scale(0.3) translate3d(0,0,0);
-      }
-    }
-    @media only screen and (max-width : 750px) {.ww-widget{width:85%;bottom:0;left:0;}}
 
-    .editor{display: none;}
-    .pell-content{height: 250px !important;}
-    .pell-content, .pell-content p{text-align: left; font-family: Helvetica, sans-serif; font-size: 17px; line-height: 26px; font-weight: 400;}
-    #edit-text{margin-bottom: 75px;}
-
-    ${editable_title}{cursor:pointer;border:1px solid transparent;}
-    h1:hover:not(.exclude),h2:hover:not(.exclude),h3:hover:not(.exclude),h4:hover:not(.exclude),h5:hover:not(.exclude){border:1px solid lightblue;}
-    ${editable_paragraph}:not(.exclude){cursor:pointer;border:1px solid transparent;}
-    ${editable_paragraph}:hover:not(.exclude){border:1px solid lightblue;}
-    .current-item{border:1px solid lightblue;}
-    div.current-item{border: none;}
-    #widget p{border: none;}
-    </style>`;
 
     document.body.innerHTML += `
     <div id="widget" class="ww-widget" spellCheck="false">
@@ -170,7 +122,7 @@ function createWidget() {
 
       
       </div>
-      <div class="ww-footer"><a href="#" class="ww-button ww-button-close">OK</a></div>
+      <div class="ww-footer"><a class="ww-button ww-button-close">OK</a></div>
     </div>`;
 
     document.querySelector('.ww-close').addEventListener('click', function() {
